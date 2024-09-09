a10cba0aa265da34c0c8f8c26e9bbc33731e69ac
Yuna Watanabe <yuna-watanabe1923@g.ecc.u-tokyo.ac.jp>
2024-09-09 16:38:04 -0400

    Added 2.10 Image Tag

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -161,7 +161,19 @@ export default function Lab1() {
                 </tfoot>
                 </table>
             </div>
+            <div id="wd-images">
+                <h4>Image tag</h4>
+                Loading an image from the internet:
+                <br />
+                <img id="wd-starship"
+                    width="400px"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+                />
+                <br />
+                Loading a local image:
+                <br />
+                <img id="wd-teslabot" src="images/teslabot.jpg" height="200px" />
+            </div>
         </div>
     );
   }
-  
