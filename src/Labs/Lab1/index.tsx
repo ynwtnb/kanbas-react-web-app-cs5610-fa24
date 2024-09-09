a442c8832cb5b89eae21819357e6969df7a5b491
Yuna Watanabe <yuna-watanabe1923@g.ecc.u-tokyo.ac.jp>
2024-09-09 16:16:13 -0400

    Added 2.9 Table Tags

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -80,8 +80,88 @@ export default function Lab1() {
                         <li>Atomic Habits</li>
                     </ul>
             </div>
+            <div id="wd-tables">
+                <h4>Table Tag</h4>
+                <table border={1} width="100%">
+                <thead>
+                    <tr>
+                        <th>Quiz</th>
+                        <th>Topic</th>
+                        <th>Date</th>
+                        <th>Grade</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Q1</td>
+                        <td>HTML</td>
+                        <td>2/3/21</td>
+                        <td>85</td>
+                    </tr>
+                    <tr>
+                        <td>Q2</td>
+                        <td>CSS</td>
+                        <td>2/10/21</td>
+                        <td>90</td>
+                    </tr>
+                    <tr>
+                        <td>Q3</td>
+                        <td>CSS</td>
+                        <td>2/17/21</td>
+                        <td>95</td>
+                    </tr>
+                    <tr>
+                        <td>Q4</td>
+                        <td>JavaScript</td>
+                        <td>2/24/21</td>
+                        <td>87</td>
+                    </tr>
+                    <tr>
+                        <td>Q5</td>
+                        <td>A1-A3</td>
+                        <td>3/3/21</td>
+                        <td>95</td>
+                    </tr>
+                    <tr>
+                        <td>Q6</td>
+                        <td>Redux</td>
+                        <td>3/10/21</td>
+                        <td>92</td>
+                    </tr>
+                    <tr>
+                        <td>Q7</td>
+                        <td>Redux</td>
+                        <td>3/17/21</td>
+                        <td>89</td>
+                    </tr>
+                    <tr>
+                        <td>Q8</td>
+                        <td>Node</td>
+                        <td>3/24/21</td>
+                        <td>82</td>
+                    </tr>
+                    <tr>
+                        <td>Q9</td>
+                        <td>Node</td>
+                        <td>3/31/21</td>
+                        <td>88</td>
+                    </tr>
+                    <tr>
+                        <td>Q10</td>
+                        <td>Mongo</td>
+                        <td>4/7/21</td>
+                        <td>100</td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                    <td colSpan={3}>Average</td>
+                    <td>82.3</td>
+                    </tr>
+                </tfoot>
+                </table>
+            </div>
         </div>
-      
     );
   }
   
