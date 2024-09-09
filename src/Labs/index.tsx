6401fdfc9a457202edd73557e7b727afaa21f2ea
Yuna Watanabe <yuna-watanabe1923@g.ecc.u-tokyo.ac.jp>
2024-09-09 15:27:57 -0400

    Connected Labs, Apps, and Index

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -2,7 +2,7 @@ import Lab1 from "./Lab1";
 export default function Labs() {
     return (
         <div id = "wd-labs">
-            <h2>Labs</h2>
+            <h1>Labs</h1>
             <Lab1 />
         </div>
     );
