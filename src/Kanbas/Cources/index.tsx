7a4456ec2417dc945e6f7cc86f1d1732490c62ca
Yuna Watanabe <yuna-watanabe1923@g.ecc.u-tokyo.ac.jp>
2024-09-16 19:11:05 -0400

    Added titles

--- src/Kanbas/Cources/index.tsx
+++ src/Kanbas/Cources/index.tsx
@@ -23,6 +23,10 @@ export default function Courses() {
                         <Route path="Modules" element={<Modules />} />
                         <Route path="Assignments" element={<Assignments />} />
                         <Route path="Assignments/:aid" element={<AssignmentEditor />} />
+                        <Route path="Quizzes" element={<h2>Quizzes</h2>} />
+                        <Route path="Zoom" element={<h2>Zoom</h2>} />
+                        <Route path="Grades" element={<h2>Grades</h2>} />
+                        <Route path="Piazza" element={<h2>Piazza</h2>} />
                         <Route path="People" element={<h2>People</h2>} />
                         </Routes>
                     </td>
