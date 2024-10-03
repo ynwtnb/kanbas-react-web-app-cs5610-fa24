4161530bf41f982e8f3281ba4bc94c351d33d9b1
Yuna Watanabe <yuna-watanabe1923@g.ecc.u-tokyo.ac.jp>
2024-10-03 13:41:47 -0400

    Updated id of github link:

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -33,7 +33,7 @@ export default function TOC() {
                 </a>
             </li>
             <li className="nav-item">
-                <a id="wd-k" href="https://github.com/ynwtnb/kanbas-react-web-app" className="nav-link">
+                <a id="wd-github" href="https://github.com/ynwtnb/kanbas-react-web-app" className="nav-link">
                 My GitHub
                 </a>
             </li>
