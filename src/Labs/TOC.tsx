a206d64676d448892308ee8fe7c9284d9a35f33d
Yuna Watanabe <yuna-watanabe1923@g.ecc.u-tokyo.ac.jp>
2024-10-17 11:27:40 -0400

    completed Lab3

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -4,35 +4,35 @@ export default function TOC() {
     const { pathname } = useLocation();
     return (
         <ul className="nav nav-pills">
-            <li className="nav-item">
+            <li className="nav-link">
                 <a id="wd-a" href="#/Labs" className="nav-link">
                 Labs
                 </a>
             </li>
-            <li className="nav-item">
+            <li className="nav-link">
                 <a id="wd-a1" href="#/Labs/Lab1"
                 className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}>
                 Lab 1
                 </a>
             </li>
-            <li className="nav-item">
+            <li className="nav-link">
                 <a id="wd-a2" href="#/Labs/Lab2"
                 className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>
                 Lab 2
                 </a>
             </li>
-            <li className="nav-item">
+            <li className="nav-link">
                 <a id="wd-a3" href="#/Labs/Lab3"
                 className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>
                 Lab 3
                 </a>
             </li>
-            <li className="nav-item">
+            <li className="nav-link">
                 <a id="wd-k" href="#/Kanbas" className="nav-link">
                 Kanbas
                 </a>
             </li>
-            <li className="nav-item">
+            <li className="nav-link">
                 <a id="wd-github" href="https://github.com/ynwtnb/kanbas-react-web-app" className="nav-link">
                 My GitHub
                 </a>
