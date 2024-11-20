d255119a4ede527a55b65ab7d520b8a8d1c90a85
Yuna Watanabe <yuna-watanabe1923@g.ecc.u-tokyo.ac.jp>
2024-11-20 14:58:27 -0500

    implemented RESTful Web API for Account & enabled sessions

--- src/Kanbas/Account/Signin.tsx
+++ src/Kanbas/Account/Signin.tsx
@@ -1,30 +1,53 @@
+import * as client from "./client";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { setCurrentUser } from "./reducer";
-import * as db from "../Database";
 import React from "react";
 
 export default function Signin() {
-  const [credentials, setCredentials] = useState<any>({});
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const signin = () => {
-    const user = db.users.find(
-      (u: any) => u.username === credentials.username && u.password === credentials.password);
-    if (!user) return;
-    dispatch(setCurrentUser(user));
-    navigate("/Kanbas/Dashboard");
-  };
-  return (
-    <div id="wd-signin-screen">
-      <h3>Sign in</h3>
-      <input value = {credentials.username}
-        onChange={(e) => setCredentials({...credentials, username: e.target.value})} id="wd-username" placeholder="username" className="form-control mb-2"/>
-      <input value={credentials.password} 
-        onChange={(e) => setCredentials({...credentials, password: e.target.value})} id="wd-password" placeholder="password" type="password" className="form-control mb-2" />
-      <button onClick={signin} id="wd-signin-btn"
-              className="btn btn-primary w-100"> Sign in </button>
-      <Link  id="wd-signup-link" to="/Kanbas/Account/Signup">Sign up</Link>
-    </div>
-);}
+	const [credentials, setCredentials] = useState<any>({});
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const signin = async () => {
+		const user = await client.signin(credentials);
+		if (!user) return;
+		dispatch(setCurrentUser(user));
+		navigate("/Kanbas/Dashboard");
+	};
+	return (
+		<div id="wd-signin-screen">
+			<h3>Sign in</h3>
+			<input
+				value={credentials.username}
+				onChange={(e) =>
+					setCredentials({ ...credentials, username: e.target.value })
+				}
+				id="wd-username"
+				placeholder="username"
+				className="form-control mb-2"
+			/>
+			<input
+				value={credentials.password}
+				onChange={(e) =>
+					setCredentials({ ...credentials, password: e.target.value })
+				}
+				id="wd-password"
+				placeholder="password"
+				type="password"
+				className="form-control mb-2"
+			/>
+			<button
+				onClick={signin}
+				id="wd-signin-btn"
+				className="btn btn-primary w-100 mb-2"
+			>
+				{" "}
+				Sign in{" "}
+			</button>
+			<Link id="wd-signup-link" to="/Kanbas/Account/Signup">
+				Sign up
+			</Link>
+		</div>
+	);
+}
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,4 +1,4 @@
-import "./styles.css"
+import "./styles.css";
 import Account from "./Account";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses";
@@ -9,56 +9,81 @@ import { useState } from "react";
 import ProtectedRoute from "./Account/ProtectedRoute";
 import ProtectedCourse from "./ProtectedCourse";
 import React from "react";
+import Session from "./Account/Session";
 
 export default function Kanbas() {
-    const [courses, setCourses] = useState<any[]>(db.courses);
-    const [course, setCourse] = useState<any>({
-        _id: "0", name: "New Course", number: "New Number",
-        startDate: "2023-09-10", endDate: "2023-12-15",
-        image: "/images/reactjs.jpg", description: "New Description"
-    });
-    const addNewCourse = () => {
-        const newCourse = { ...course,
-                            _id: new Date().getTime().toString() };
-        setCourses([...courses, { ...course, ...newCourse }]);
-    };
-    const deleteCourse = (courseId: string) => {
-        setCourses(courses.filter((course) => course._id !== courseId));
-    };
-    const updateCourse = () => {
-        setCourses(
-            courses.map((c) => {
-            if (c._id === course._id) {
-                return course;
-            } else {
-                return c;
-            }
-            })
-        );
-    };
-    return (
-        <div id="wd-kanbas">
-            <KanbasNavigation />
-            <div className="wd-main-content-offset p-3">
-                <Routes>
-                    <Route path="/" element={<Navigate to="Dashboard" />} />
-                    <Route path="/Account/*" element={<Account />} />
-                    <Route path="/Dashboard" element={<ProtectedRoute>
-                                                        <Dashboard 
-                                                            courses={courses}
-                                                            course={course}
-                                                            setCourse={setCourse}
-                                                            addNewCourse={addNewCourse}
-                                                            deleteCourse={deleteCourse}
-                                                            updateCourse={updateCourse}
-                                                        />
-                                                    </ProtectedRoute>} />
-                    <Route path="/Courses" element={<ProtectedRoute><Navigate to="Courses" /></ProtectedRoute>} />
-                    <Route path="/Courses/:cid/*" element={<ProtectedCourse><Courses courses={courses} /></ProtectedCourse>} />
-                    <Route path="/Calendar" element={<h1>Calendar</h1>} />
-                    <Route path="/Inbox" element={<h1>Inbox</h1>} />
-                </Routes>
-            </div>
-        </div>
-    );
+	const [courses, setCourses] = useState<any[]>(db.courses);
+	const [course, setCourse] = useState<any>({
+		_id: "0",
+		name: "New Course",
+		number: "New Number",
+		startDate: "2023-09-10",
+		endDate: "2023-12-15",
+		image: "/images/reactjs.jpg",
+		description: "New Description",
+	});
+	const addNewCourse = () => {
+		const newCourse = { ...course, _id: new Date().getTime().toString() };
+		setCourses([...courses, { ...course, ...newCourse }]);
+	};
+	const deleteCourse = (courseId: string) => {
+		setCourses(courses.filter((course) => course._id !== courseId));
+	};
+	const updateCourse = () => {
+		setCourses(
+			courses.map((c) => {
+				if (c._id === course._id) {
+					return course;
+				} else {
+					return c;
+				}
+			})
+		);
+	};
+	return (
+		<Session>
+			<div id="wd-kanbas">
+				<KanbasNavigation />
+				<div className="wd-main-content-offset p-3">
+					<Routes>
+						<Route path="/" element={<Navigate to="Dashboard" />} />
+						<Route path="/Account/*" element={<Account />} />
+						<Route
+							path="/Dashboard"
+							element={
+								<ProtectedRoute>
+									<Dashboard
+										courses={courses}
+										course={course}
+										setCourse={setCourse}
+										addNewCourse={addNewCourse}
+										deleteCourse={deleteCourse}
+										updateCourse={updateCourse}
+									/>
+								</ProtectedRoute>
+							}
+						/>
+						<Route
+							path="/Courses"
+							element={
+								<ProtectedRoute>
+									<Navigate to="Courses" />
+								</ProtectedRoute>
+							}
+						/>
+						<Route
+							path="/Courses/:cid/*"
+							element={
+								<ProtectedCourse>
+									<Courses courses={courses} />
+								</ProtectedCourse>
+							}
+						/>
+						<Route path="/Calendar" element={<h1>Calendar</h1>} />
+						<Route path="/Inbox" element={<h1>Inbox</h1>} />
+					</Routes>
+				</div>
+			</div>
+		</Session>
+	);
 }
