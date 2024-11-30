dcd8f05554803f594e77a2d1c876943cb9ded409
Yuna Watanabe <yuna-watanabe1923@g.ecc.u-tokyo.ac.jp>
2024-11-30 18:08:12 -0500

    implemented a6 4.1 and above

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -1,6 +1,7 @@
 import Signin from "./Signin";
 import Profile from "./Profile";
 import Signup from "./Signup";
+import Users from "./Users";
 import AccountNavigation from "./Navigation";
 import {Routes, Route, Navigate} from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -21,6 +22,8 @@ export default function Account() {
               <Route path="/Signin" element={<Signin />} />
               <Route path="/Profile" element={<Profile />} />
               <Route path="/Signup" element={<Signup />} />
+              <Route path='/Users' element={<Users />} />
+              <Route path='/Users/:uid' element={<Users />} />
           </Routes>  
         </div>
       </div>
--- src/Kanbas/Courses/People/Table.tsx
+++ src/Kanbas/Courses/People/Table.tsx
@@ -1,25 +1,32 @@
 import { FaUserCircle } from "react-icons/fa";
-import { useParams } from "react-router-dom";
-import * as db from "../../Database";
+// import { useParams } from "react-router-dom";
+// import * as db from "../../Database";
 import React from "react";
+import PeopleDetails from "./Details";
+import { Link } from "react-router-dom";
 
-export default function PeopleTable() {
-    const { cid } = useParams();
-    const { users, enrollments } = db;
+export default function PeopleTable({ users = [] }: { users?: any[] }) {
+    // const { cid } = useParams();
+    // const { users, enrollments } = db;
     return (
         <div id="wd-people-table">
+            <PeopleDetails />
         <table className="table table-striped">
             <thead>
             <tr><th>Name</th><th>Login ID</th><th>Section</th><th>Role</th><th>Last Activity</th><th>Total Activity</th></tr>
             </thead>
             <tbody>
-            {users.filter((usr) => enrollments.some((enrollment) => enrollment.user === usr._id && enrollment.course === cid))
+            {users
+            // .filter((usr) => enrollments.some((enrollment) => enrollment.user === usr._id && enrollment.course === cid))
             .map((user: any) => (
                 <tr key = {user._id}>
                     <td className="wd-full-name text-nowrap">
-                        <FaUserCircle className="me-2 fs-1 text-secondary" />
-                    <span className="wd-first-name">{user.firstName}</span>{" "}
-                    <span className="wd-last-name">{user.lastName}</span></td>
+                        <Link to={`/Kanbas/Account/Users/${user._id}`} className="text-decoration-none">
+                            <FaUserCircle className="me-2 fs-1 text-secondary" />
+                            <span className="wd-first-name">{user.firstName}</span>{" "}
+                            <span className="wd-last-name">{user.lastName}</span>
+                        </Link>
+                    </td>
                     <td className="wd-login-id">{user.loginId}</td>
                     <td className="wd-section">{user.section}</td>
                     <td className="wd-role">{user.role}</td>
